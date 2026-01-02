3fd32d3358f95e39f548aa85de7408ad6ac7d440
HamishM58 <hamishfmitchell@gmail.com>
2026-01-02 17:18:00 +1100

    Update SRT_OpenCan.h

--- lib/SRT_OpenCan/SRT_OpenCan.h
+++ lib/SRT_OpenCan/SRT_OpenCan.h
@@ -14,15 +14,14 @@ public:
     SRT_CanOpenMtr(int (*send_func)(uint16_t, uint8_t, uint8_t*, bool), uint8_t node_id);
     int process_msg(uint16_t can_id, uint8_t len, uint8_t* data);
     int enable_motor();
-    int move_relative(int32_t steps, uint32_t accel_ms = 1000, uint32_t decel_ms = 1000);
+    int move_relative(int32_t steps,uint32_t velocity, uint32_t accel_ms = 1000, uint32_t decel_ms = 1000);
+    int move_absolute(int32_t target,uint32_t velocity, uint32_t accel_ms = 1000, uint32_t decel_ms = 1000);
 
-    static void handleSerialCommand(const String &cmd, SRT_CanOpenMtr* motors, size_t num_Openmotors, 
-                                   const uint8_t* node_ids, uint32_t accel_ms, uint32_t decel_ms);
-    static String readSerialLine();
+    int stop();
+    int Estop();
 
     //=========== Homing attempts =========
     int do_homing(); // run homing sequence, set current pos = 0
-    int move_absolute(int32_t target, uint32_t accel_ms = 1000, uint32_t decel_ms = 1000);
 };
 
 #endif
